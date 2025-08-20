f365d6e0ff97bb5e51bc97f258fe87d5ef47a907
Alson Dabo <alsondab9@gmail.com>
2025-08-20 19:09:34 +0000

    detail page product galery

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,6 +3,7 @@
 import { connectToDatabase } from '@/lib/db'
 import Product from '@/lib/db/models/product.model'
 import { IProduct } from '@/lib/db/models/product.model'
+import { PAGE_SIZE } from '@/lib/constants'
 
 // CodeGen: Refactor | Explain | Generate JSDoc | X
 export async function getAllCategories() {
@@ -139,3 +140,40 @@ export async function getDealProducts(limit = 10) {
     .limit(limit)
   return JSON.parse(JSON.stringify(products)) as IProduct[]
 }
+
+   // GET ONE PRODUCT BY SLUG
+   export async function getProductBySlug(slug: string) {
+    await connectToDatabase()
+    const product = await Product.findOne({ slug, isPublished: true })
+    if (!product) throw new Error('Product not found')
+    return JSON.parse(JSON.stringify(product)) as IProduct
+  }
+  // GET RELATED PRODUCTS: PRODUCTS WITH SAME CATEGORY
+  export async function getRelatedProductsByCategory({
+    category,
+    productId,
+    limit = PAGE_SIZE,
+    page = 1,
+  }: {
+    category: string
+    productId: string
+    limit?: number
+    page: number
+  }) {
+    await connectToDatabase()
+    const skipAmount = (Number(page) - 1) * limit
+    const conditions = {
+      isPublished: true,
+      category,
+      _id: { $ne: productId },
+    }
+    const products = await Product.find(conditions)
+      .sort({ numSales: 'desc' })
+      .skip(skipAmount)
+      .limit(limit)
+    const productsCount = await Product.countDocuments(conditions)
+    return {
+      data: JSON.parse(JSON.stringify(products)) as IProduct[],
+      totalPages: Math.ceil(productsCount / limit),
+    }
+  }
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,5 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'MendelCorp';
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Dépensez moins, profitez plus.';
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Un site e-commerce réalisé avec Next.js et MongoDB';
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
